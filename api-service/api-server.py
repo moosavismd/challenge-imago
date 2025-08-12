af6b04c3202522eb5f181dc606c41fde325600e8
moosavismd <moosavi.smd@arvancloud.ir>
2025-08-12 18:07:05 +0330

    trying ffixing cicd pipeline 2

--- api-service/api-server.py
+++ api-service/api-server.py
@@ -22,7 +22,7 @@ def random_number():
 def constant_number():
     """Return the constant number 17"""
     return jsonify({
-        'number': 22,
+        'number': 17,
         'endpoint': '/cons'
     })
 
